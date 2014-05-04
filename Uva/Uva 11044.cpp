495cbb28813fa9c6460ec1a8f9c06da550c6cd64
ryuxin <m.phelps.a@gmail.com>
2014-05-04 10:46:54 +0800

    add Uva 11044

---
+++ Uva/Uva 11044.cpp
@@ -0,0 +1,18 @@
+﻿/*入门题。简单计算*/
+#include <iostream>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <stdio.h>
+#include <queue>
+using namespace std;
+int main()
+{
+	int t, m, n;
+	scanf("%d", &t);
+	while(t--) {
+		scanf("%d %d", &m, &n);
+		printf("%d\n", (m/3)*(n/3));
+	}
+	return 0;
+}	
